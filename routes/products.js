0c48e3f79a1c90a0296729837f9b29bdb34d98de
MOHAMMEDSAGHEER95 <mohammedsagheer95@gmail.com>
2022-09-19 11:49:20 +0530

    delete and edit done

--- routes/products.js
+++ routes/products.js
@@ -20,6 +20,16 @@ router.post('/', async (req, res) => {
     res.send('product saved')
 })
 
+router.post('/edit/:id', async (req, res) => {
+    await Products.findByIdAndUpdate(req.params['id'], {price: req.body.price})
+    res.send(`product edit ${req.params['id']}`)
+})
 
+
+
+router.delete('/delete/:id', async (req, res) => {
+    await Products.findByIdAndDelete(req.params['id'])
+    res.send(`products deleted ${req.params['id']}`)
+})
 
 module.exports = router
